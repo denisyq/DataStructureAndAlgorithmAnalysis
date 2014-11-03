adc0cce3309ffd8db663112ee9f68d4a4cb27c86
denisyq <denisyq@gmail.com>
2014-11-03 18:15:22 +0800

    update ..

--- a.cc
+++ a.cc
@@ -13,37 +13,82 @@ using namespace std;
         {0,0,2,0,5,0,1},
         {0,0,0,6,5,1,0} };
 
+int dataDirection[7][7]={
+    {0,0,0,0,0,0,0},
+    {0,0,1,1,0,0,0},
+    {0,0,0,0,1,1,0},
+    {0,0,0,0,0,0,1},
+    {0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,0},
+};
+
 
 
 class BFS{
-	public:
-		BFS():N_(7){
-			visited_.assign(0,N_);	
+public:
+		BFS():n_(7){
+			visited_.assign(n_,0);	//first arg is num, sec is value
 		}
 		void work(int start){
-			if(!visited_[start])
+			if(!visited_[start]){
+                //push in queue, set visited[]
+                //do move visited_[start]=1 to queue.pop
+                //in case duplicate in queue.
 				iq_.push(start);
+                visited_[start]=true;
+            }
 			while(iq_.size()!=0){
-				cout<<" while"<<endl;
 				int tmp=iq_.front();
 				iq_.pop();
-				visited_[tmp]=1;
-				cout<<"-->"<<tmp;
-				for(int i=1;i<N_;++i){
-					if(data[tmp][i]!=0 && !visited_[i])
+				cout<<"print  "<<tmp<<endl;
+				for(int i=1;i<n_;++i){
+					if(dataDirection[tmp][i]!=0 && !visited_[i]){
 						iq_.push(i);
+                        visited_[i]=true;//see comments in line_25
+                    }
 				}
 			}
 		}
 private:
 		vector<int> visited_;
 		queue<int> iq_;
-		int N_;
+		int n_;
 };
+
+class DFS{
+public:
+    DFS():n_(7){
+        visited_.assign(n_,0); 
+    }
+    void work(int start){
+        //print start here to get positive traverse
+        //cout<<"print "<<start<<endl;
+        visited_[start]=true;
+        //use recursive algo to hide stack operation.
+        //DFS has another non recursive realization.
+        for(int i=1;i<n_;++i){
+            if(dataDirection[start][i]!=0 && !visited_[i])
+                work(i);
+        }
+        //print start here to get reverse traverse
+        //while this traverse could be used to topology sort.
+        //cout<<"print "<<start<<endl;
+    }
+private:
+    vector<int> visited_;
+    int n_;
+
+};
+
+
 
 int main(void){
 	BFS bfs;
 	bfs.work(1);
+    cout<<" -------------"<<endl;
+    DFS dfs;
+    dfs.work(1);
     return 0;
 }
 
