94a9125947ad798e3a6af2ae64c634ef403bfe7a
denisyq <denisyq@gmail.com>
2014-11-18 18:24:12 +0800

    modify a.cc

--- a.cc
+++ a.cc
@@ -13,128 +13,64 @@ int data[7][7]={
     {0,0,2,0,5,0,1},
     {0,0,0,6,5,1,0} };
 
-int dataDirection[7][7]={
-    {0,0,0,0,0,0,0},
-    {0,0,1,1,0,0,0},
-    {0,0,0,0,1,1,0},
-    {0,0,0,0,0,0,1},
-    {0,0,0,0,0,0,0},
-    {0,0,0,1,0,0,0},
-    {0,0,0,0,0,0,0},
-};
+typedef struct node{
+    int u;
+    int v;
+    int cost;
+}Node;
 
-
-class BFS{
+class BellmanFord{
 public:
-		BFS():n_(7){
-			visited_.assign(n_,0);	//first arg is num, sec is value
-		}
-		void work(int start){
-			if(!visited_[start]){
-                //push in queue, set visited[]
-                //do move visited_[start]=1 to queue.pop
-                //in case duplicate in queue.
-				iq_.push(start);
-                visited_[start]=true;
-            }
-			while(iq_.size()!=0){
-				int tmp=iq_.front();
-				iq_.pop();
-				cout<<"print  "<<tmp<<endl;
-				for(int i=1;i<n_;++i){
-					if(dataDirection[tmp][i]!=0 && !visited_[i]){
-						iq_.push(i);
-                        visited_[i]=true;//see comments at work() function begin.
-                    }
-				}
-			}
-		}
-private:
-		vector<int> visited_;
-		queue<int> iq_;
-		int n_;
-};
-
-class DFS{
-public:
-    DFS():n_(7),isLoop_(false){
-        visited_.assign(n_,0); 
+    BellmanFord(int n,int src):vertex_(n),src_(src){
+        dist_.assign(vertex_,999);
+        visited_.assign(vertex_+1,0);
+        path_.assign(vertex_+1,0);
     }
-    void dfsRecursive(int start){
-        //print start here to get positive traverse
-        //cout<<"print "<<start<<endl;
-        visited_[start]=true;
-        //use recursive algo to hide stack operation.
-        for(int i=1;i<n_;++i){
-            if(dataDirection[start][i]!=0 && !visited_[i])
-                dfsRecursive(i);
-        }
-        //print start here to get reverse traverse
-        //while this traverse could be used to topology sort.
-        //cout<<"print "<<start<<endl;
-    }
-    void dfsNoRecursive(int start){
-        visited_[start]=true;
-        s_.push(start);
-        while(!s_.empty()){
-            int tmp=s_.top();
-            s_.pop();
-            //cout<<"print "<<tmp<<endl;
-            for(int i=1;i<n_;++i){
-                if(dataDirection[tmp][i] != 0 && !visited_[i]){
-                    s_.push(i);
-                    visited_[i]=true;
+    bool work(){
+        dist_[src_]=0;
+        for(int i=1;i<=vertex_-1;++i){
+            for(int j=1;j<=NUM_EDGE;++j){
+                if(dist_[edge[j].u]+edge[j].cost < dist_[edge[j].v]){
+                    dist_[edge[j].v] = dist_[edge[j].u]+edge[j].cost;
+                    path_[edge[j].v] = edge[j].u;
                 }
             }
         }
-    }
-
-
-    //DFS can detect the loop.
-    //visted_[i] == -1 and then set to true
-
-    bool dfsCheckLoop(int start){
-        visited_[start]=-1;//set -1, not true;
-        for(int i=1;i<n_;++i){
-            if(dataDirection[start][i] != 0){
-                //NOTE: == -1 and return is only take effect on the recursive dfsCheckLoop(),
-                //not the first dfsCheckLoop. Use the "if(dfsCheckLoop()) return true" to pass one by one.
-                //careful about the "return" in recursive func. 
-                if(visited_[i] == -1) 
-                    return true;
-                else if(!visited_[i])
-                    if(dfsCheckLoop(i)) return true;
-                    //if not return, will always return false at the end of func.
+        bool flag=true;
+        for(int i=1;i<=NUM_EDGE;++i){
+            if(dist_[edge[j].u]+edge[j].cost < dist_[edge[j].v]){
+                flag=false;
+                break;
             }
         }
-        visited_[start]=true;
-        return false;
+        return flag;
+        
     }
-    //set isLoop_ and create a new api to get isLoop_
-    void dfsCheckLoop2(int start){
-        visited_[start]=-1;//set -1, not true;
-        for(int i=1;i<n_;++i){
-            if(dataDirection[start][i] != 0){
-                if(visited_[i] == -1){
-                    isLoop_ =true;
-                    break;
-                }
-                else if(!visited_[i])
-                    dfsCheckLoop2(i);
-            }
-        }
-        visited_[start]=true;
-    }
-    bool isLoop(void){
-        return isLoop_;
-    }
+
+    void printPath(int index){
+        stack<int> s;
+        int i = index;
+        s.push(i);
+        while(path_[i]!=src_){
+            s.push(path_[i]);
+            i = path_[i];
+        }   
+        s.push(src_);
+        //data ready, print the stack
+        while(!s.empty()){
+            cout<<"->"<<s.top();
+            s.pop();
+        }   
+        cout<<endl;
+    }   
+ 
 private:
+    int vertex_;
+    int src_;
+    vector<int> dist_;
     vector<int> visited_;
-    stack<int> s_;//store non recursive DFS
-    int n_;
-    bool isLoop_;
+    vector<int> path_;
 };
-
 
 
 int main(void){
