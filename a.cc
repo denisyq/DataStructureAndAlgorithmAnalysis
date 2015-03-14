d3c07cc3789c0ee0c2efe6dd7a1c6dcf80f753c3
denisyq <denisyq@gmail.com>
2015-03-14 17:53:24 +0800

    update

--- a.cc
+++ a.cc
@@ -1,226 +1,268 @@
 #include <iostream>
-#include <vector>
-#include <stack>
+#include <cstdio>
 #include <algorithm>
-#include <climits>
+#include <map>
+#include <set>
+#include <vector>
+#include <list>
+#include <string>
 #include <queue>
-#include <memory.h>
+#include <deque>
 using namespace std;
 
-const int RED = 1;
-const int BLACK = 2;
+typedef struct Meeting{
+    int from;
+    int end;
+    int tag;
+    int room;
+    friend bool operator < (struct Meeting const &a, struct Meeting const &b) {
+        if(a.end != b.end)
+            return a.end < b.end;
+        else
+            return a.from<b.from;
+    }
+    friend bool operator > (struct Meeting const &a, struct Meeting const &b) {
+        if(a.end != b.end)
+            return a.end > b.end;
+        else
+            return a.from>b.from;
+    }
+}Meeting;
 
-typedef struct node{
-    int key;
-    int color;
-    struct node *left;
-    struct node *right;
-    struct node *parent;
-}Node;
+bool bigger(Meeting const& a, Meeting const& b){
+    if(a.end != b.end)
+        return a.end > b.end;
+    else
+        return a.from>b.from;
+}
+void printMeeting(Meeting meeting){
+    cout<<meeting.tag<<" "<<meeting.from<<" "<<meeting.end<<endl;
+}
 
-class RBTree{
-    public:
-        RBTree();
-        ~RBTree();
-        void inOrder(void);
-        void insert(int key);
-        Node* find(int key);
-        Node* remove(int key);
-    private:
-        Node* _root;
-        void _insert(Node* node, Node* parent, int key);
-        Node* _find(Node* node, int key);
-        Node* _remove(Node* node, int key);
-        void _makeEmpty(Node* node);
-        void _inOrder(Node* node);
-		Node* _findMin(Node* node);
-		Node* _findMax(Node* node); 
-        Node* _rebalance(Node* node);
-        Node* _leftRotate(Node* node);
-        Node* _rightRotate(Node* node);
+class Solution{
+public:
+    Solution(int N):num_case(N){};
+    void fix(void){
+        for(int i=0;i<num_case;i++){
+            cout<<"Case #"<<i+1<<endl;
+            container.clear();
+            for(int k=0;k<=100;k++)
+                output[k].clear();
+            //queueMeet.clear();
+            cin>>num_meeting>>num_room;
+            for(int j=1;j<=num_meeting;j++){
+                cin>>a>>b>>c>>d;
+                meet_.from = a*100+b;
+                meet_.end = c*100+d;
+                meet_.tag = j;
+                container.push_back(meet_);
+            }
+            sort(container.begin(),container.end(),bigger );
+            //cout<<"afer sort, meeting sequence print___"<<endl;
+            //for_each(container.begin(),container.end(),printMeeting);
+
+
+            while(!container.empty()){
+                Meeting containerMeet = container.back();
+                //cout<<"begin"<<" container.from/end" <<containerMeet.from<<"  "<<containerMeet.end<<endl;
+                //cout<<queueMeet.empty()<<endl;
+                if(!queueMeet.empty()){//swap one
+                    Meeting topMeet = queueMeet.top();
+                    if(topMeet.end < containerMeet.from){
+                        //cout<<"swap it!!!!!"<<"topMeet.end:"<<topMeet.end<<" container.from"<<containerMeet.from<<endl;
+                        containerMeet.room = topMeet.room;
+                        save(topMeet.room,topMeet.tag);
+                        queueMeet.pop();
+                        queueMeet.push(containerMeet);
+                    }
+                    else if(queueMeet.size() == num_room){
+                        //cout<<"drop it!!!"<<endl;
+                    }
+                    else{
+                        //cout<<"push into queue!!"<<"from="<<containerMeet.from<<"end="<<containerMeet.end<<endl;
+                        containerMeet.room = queueMeet.size()+1;
+                        queueMeet.push(containerMeet);
+                    }
+                }
+                else{//push one
+                    //cout<<"push into queue!!"<<"from="<<containerMeet.from<<"end="<<containerMeet.end<<endl;
+                    containerMeet.room = queueMeet.size()+1;
+                    queueMeet.push(containerMeet);
+                }
+                container.pop_back();
+                /*
+                priority_queue< Meeting,vector<Meeting>,greater<Meeting> > copyone(queueMeet);
+                while(!copyone.empty()){
+                    cout<<"in queue "<<copyone.top().from<<" "<<copyone.top().end<<endl;
+                    copyone.pop();
+                }
+                */
+            }
+            while(!queueMeet.empty()){
+                Meeting topMeet = queueMeet.top();
+                save(topMeet.room,topMeet.tag);
+                queueMeet.pop();
+            }
+            //print output
+            for(int i=1;i<=100;i++){
+                if(!output[i].empty()){
+                    string s = output[i];
+                    s.erase(s.end()-1);
+                    cout<<s<<endl;
+                }
+                else break;
+            }
+
+        }
+    }
+
+
+    void fix_2(void){
+        for(int i=0;i<num_case;i++){
+            cout<<"Case #"<<i+1<<endl;
+            container.clear();
+            for(int k=0;k<=100;k++)
+                output[k].clear();
+            //queueMeet.clear();
+            cin>>num_meeting>>num_room;
+            for(int j=1;j<=num_meeting;j++){
+                cin>>a>>b>>c>>d;
+                meet_.from = a*100+b;
+                meet_.end = c*100+d;
+                meet_.tag = j;
+                container.push_back(meet_);
+            }
+            sort(container.begin(),container.end(),bigger );
+            //cout<<"afer sort, meeting sequence print___"<<endl;
+            //for_each(container.begin(),container.end(),printMeeting);
+
+            int last_end=9999;
+
+            int room=1;
+        again:
+            if(room>num_room) goto end;
+            while(!container.empty()){
+
+                //memset((void*)tmp,0,sizeof(Meeting));
+                Meeting containerMeet = container.back();
+                //cout<<"begin"<<" container.from/end" <<containerMeet.from<<"  "<<containerMeet.end<<endl;
+                //cout<<queueMeet.empty()<<endl;
+                if(last_end == 9999){
+                    last_end = containerMeet.end;
+                    save_2(room,containerMeet.tag);
+                    container.pop_back();
+                }else{
+                    if(last_end < containerMeet.from){
+                        last_end = containerMeet.end;
+                        save_2(room,containerMeet.tag);
+                        container.pop_back();
+                    }else{
+                        copyContainer.push_front(containerMeet);
+                        container.pop_back();
+                    }
+                }
+
+            }
+
+            output_2.erase(output_2.end()-1);
+            //cout<<"print one line"<<endl;
+            cout<<output_2<<endl;
+            output_2.clear();
+            if(!copyContainer.empty()){
+                //for_each(copyContainer.begin(),copyContainer.end(),printMeeting);
+                vector<Meeting> tmp(copyContainer.begin(),copyContainer.end());
+                copyContainer.clear();
+                container.swap(tmp);
+                room++;
+                last_end = 9999;
+                //cout<<"goto again"<<endl;
+                goto again;
+            }
+        end:
+            //cout<<"another case"<<endl;
+            output_2.clear();
+            container.clear();
+            copyContainer.clear();
+
+        }
+    }
+    void fix_3(void){//graph color
+        for(int i=0;i<num_case;i++){
+            cout<<"Case #"<<i+1<<endl;
+            container.clear();
+            cin>>num_meeting>>num_room;
+            for(int j=1;j<=num_meeting;j++){
+                cin>>a>>b>>c>>d;
+                meet_.from = a*100+b;
+                meet_.end = c*100+d;
+                meet_.tag = j;
+                container.push_back(meet_);
+            }
+            sort(container.begin(),container.end(),bigger );
+            for(int j=1;j<=container.size();j++){
+                
+            }
+        }
+    }
+private:
+    int num_case;
+    int num_room;
+    int num_meeting;
+    int a,b,c,d;
+    int from,end;
+    string output_2;
+    string output[101];
+    vector<Meeting> container;
+    deque<Meeting> copyContainer;
+    priority_queue< Meeting,vector<Meeting>,greater<Meeting> > queueMeet;
+    Meeting meet_;
+    //map<int,string> out_map;
+    string toString(int i){
+        char buf[10000];
+        sprintf(buf,"%d",i);
+        string s(buf);
+        return s;
+    }
+    void save(int room, int tag){
+        string str = toString(tag);
+        str += " ";
+        output[room] += str;
+        //cout<<"___output["<<room<<"] ="<<output[room]<<"##"<<endl;
+    }
+    void save_2(int room, int tag){
+        output_2 += toString(tag);
+        output_2 += " ";
+    }
+
 };
-RBTree::RBTree(){
-    _root = NULL;
+int main(int argc, char* argv){
+    freopen("input","r",stdin);
+    int n;
+    cin>>n;
+    Solution solution(n);
+    solution.fix_2();
 }
-RBTree::~RBTree(){
-    _makeEmpty(_root);
-}
-void RBTree::_inOrder(Node* root){
-    if(root != NULL){
-        _inOrder(root->left);
-        cout<<" "<<root<<"-"<<root->key<<endl;
-        _inOrder(root->right);
-    }
-}
-void RBTree::inOrder(void){
-    _inOrder(_root);
-}
-void RBTree::_makeEmpty(Node* root){
-    if(root == NULL) return;
-    else{
-        _makeEmpty(root->left);
-        _makeEmpty(root->right);
-    }
-    delete root;
-}
-void RBTree::insert(int key){
-    _insert(_root,NULL,key);
-}
-Node* RBTree::_leftRotate(Node* node){
-    Node* p = node->right;
-    node->right = p->left;
-    p->left = node;
-    return p;
-}
-Node* RBTree::_rightRotate(Node* node){
-    Node* p = node->left;
-    node->left = p->right;
-    p->right = node;
-    return p;
-}
-Node* RBTree::_rebalance(Node* node){
-    while(node != _root && node->parent->color == RED){
-        if(node->parent == node->parent->parent->left){//left branch
-            Node* uncle = node->parent->parent->right;
-            if(uncle && uncle->color == RED){//double RED-->change parent,uncle,grandpa, grandpa recursive
-                node->parent->color = BLACK;
-                uncle->color = BLACK;
-                node->parent->parent->color = RED;
-                node = node->parent->parent;
-            }else{//parent RED, uncle BLACK, grandpa BLACK-->change parent/grandpa,rotate
-                if(node == node->parent->right)
-                    node->parent->parent->left = _leftRotate(node->parent);
-                node->parent->color = BLACK;
-                node->parent->parent->color = RED;
-                _rightRotate(node->parent->parent);
-            }
-        }else{//right branch,similar action
-            Node* uncle = node->parent->parent->left;
-            if(uncle && uncle->color == RED){
-                node->parent->color = BLACK;
-                uncle->color = BLACK;
-                node->parent->parent->color = RED;
-                node = node->parent->parent;
-            }
-            else{
-                if(node == node->parent->left)
-                    _rightRotate(node->parent);
-                node->parent->color = BLACK;
-                node->parent->parent->color = RED;
-                _leftRotate(node->parent->parent);
-
-            }
-        }
-    }
-    _root->color = BLACK;
-
-}
-void RBTree::_insert(Node* node, Node* parent, int key){
-    while(node != NULL){
-        parent = node;
-        if(key<node->key){
-            node = node->left;
-        }else if(key>node->key){
-            node = node->right;
-        }else
-            break;
-    }
-    if(node == NULL){
-        node = new Node;
-        node->left = node->right = NULL;
-        node->parent = parent;
-        node->color = RED;
-        node->key = key;
-        if(parent == NULL) _root=node;
-        else{
-            if(node->key < node->parent->key)
-                node->parent->left = node;
-            else
-                node->parent->right = node;
-        }
-    }
-    _rebalance(node);
-}
-
-Node* RBTree::find(int key){
-    return _find(_root,key);
-}
-Node* RBTree::_find(Node* node, int key){
-    if(node == NULL) return NULL;
-    if(key < node->key)
-        return _find(node->left,key);
-    else if(key > node->key)
-        return _find(node->right,key);
-    else
-        return node;
-}
-Node* RBTree::_findMin(Node* node){
-    if(node == NULL) return node;
-    else if(node->left == NULL) return node;
-    else
-        return _findMin(node->left);
-}
-Node* RBTree::_findMax(Node* node){
-    if(node != NULL){
-        while(node->right != NULL)
-            node = node->right;
-    }
-    return node;
-}
-Node* RBTree::remove(int key){
-	_root = _remove(_root,key);
-	return _root;
-}
-Node* RBTree::_remove(Node* node, int key){
-	if(node == NULL) return NULL;
-	else if( key < node->key)
-		node->left = _remove(node->left,key);
-	else if(key > node->key)
-		node->right = _remove(node->right,key);
-	else{
-		if(node->left != NULL && node->right != NULL){
-			Node* tmp = _findMin(node->right);
-			node->key = tmp->key;
-			node->right =  _remove(node->right,node->key);
-		}
-		else{
-			Node* tmp = node;
-			if(node->left == NULL)
-				node = node->right;
-			else
-				node = node->left;
-			delete tmp;
-		}
-	}
-	return node;
-}
-int main(void){
-	RBTree bst;
-    int i=-1;
-    int a[7]={6,4,7,2,5,1,3};
-    //int a[7]={1,2,3,4,5,6,7};
-    //int a[7]={7,6,5,4,3,2,1};
-    while(i++<6){
-        bst.insert(a[i]);
-        bst.inOrder();
-        cout<<endl;
-    }
-
-    int j;
-    cout<<"delete one number"<<endl;
-    cin>>j;
-    Node* tmp = bst.remove(j);
-    if(tmp != NULL)
-        cout<<tmp->key<<endl;
-    bst.inOrder();
-
-    return 0;
-}
-
-
-
-
-
+/*
+3
+5 1
+13 20 14 0
+3 40 7 20
+12 30 13 30
+13 50 14 20
+22 10 24 0
+6 2
+13 20 14 0
+3 40 7 20
+12 30 13 30
+7 0 8 30
+22 10 24 0
+0 0 7 0
+4 2
+3 0 4 0
+3 10 4 0
+3 20 4 0
+3 50 4 0
+*/
 
 
 
